184c78fa7f81b89cd0369302d91bf29ced8db067
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2021-05-10 15:40:10 -0600

    created scripts for player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    Rigidbody2D rb;
+    float movement = 0f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement;
+        rb.velocity = velocity;
+
+    }
+}
