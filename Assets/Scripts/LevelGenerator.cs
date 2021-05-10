fe477981f4ad39973fc06a2ddacbe1f76ee2cf56
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2021-05-10 16:18:51 -0600

    made a Random Level Generator

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject platformPrefab;
+    public int numberOfPlatforms;
+    public float levelWidth = Screen.width;
+    public float minY = .8f;
+    public float maxY = 2f;
+
+    public Transform playerTransform;
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3(0f, playerTransform.position.y - .5f, 0f);
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+            spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-levelWidth/2, levelWidth/2);
+            Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
