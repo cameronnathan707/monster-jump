f72f989fd1a046e87cf1aec30381754a05b2ea3c
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2021-05-10 15:43:01 -0600

    fixed player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,17 +12,17 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movement = Input.GetAxis("Horizontal");
+        movement = Input.GetAxis("Horizontal") * movementSpeed;
     }
 
     void FixedUpdate()
     {
+        //Movement
         Vector2 velocity = rb.velocity;
         velocity.x = movement;
         rb.velocity = velocity;
