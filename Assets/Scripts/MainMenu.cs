ed20e63713b22c3dbd8fd640ca7eb387b281e01b
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2021-05-12 11:28:06 -0600

    Added a menu with buttons
    Start and Quit button are implemented but shop and settings are not.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MainMenu : MonoBehaviour
+{
+    const string GAME_SCENE = "TestScene";
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(GAME_SCENE);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Application Succesfully Shut Down!");
+        Application.Quit();
+        #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+        #endif
+    }
+
+    public void ToSettings()
+    {
+
+    }
+
+    public void ToShop()
+    {
+
+    }
+}
