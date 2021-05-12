9ad9bb4bada44e2b97478d1120b3407307d31837
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2021-05-11 23:10:35 -0600

    added quit button functionality

---
+++ Assets/QuitButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+
+public class QuitButton : MonoBehaviour
+{
+    public Button Quit;
+
+    void Start()
+    {
+        Button btn = Quit.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.Quit();
+           #if UNITY_EDITOR
+              EditorApplication.isPlaying = false;
+           #endif
+    }
+}
